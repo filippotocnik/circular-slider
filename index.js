97c3fc4341c2f8bdd626e3ef49eaffaec4290fd6
Filip Potocnik <filip.potocnik@gmail.com>
2020-08-23 22:15:00 +0200

    Fix bug

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ class Slider {
   }
 
   polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-    const angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+    const angleInRadians = (angleInDegrees * Math.PI) / 180;
 
     return {
       x: centerX + radius * Math.cos(angleInRadians),
